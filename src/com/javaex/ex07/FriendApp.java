8ad62ec048e5d255b72f605698647d679eaac81d
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-27 00:21:38 +0900

    2

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -8,25 +8,32 @@ public class FriendApp {
 
         Friend[] friendArray = new Friend[3];
         Scanner sc = new Scanner(System.in);
+        
+        Friend friend=new Friend();
+        
 
         System.out.println("친구를 3명 등록해 주세요");
 
         //친구정보 3명 입력 로직 --> 반복문 사용
         for(int i=0; i<friendArray.length; i++) {
-        	System.out.print("이름: ");
-        	friendArray[i].setName(sc.nextLine());
+        	friendArray[i]=friend;
         	
-        	System.out.println("\n핸드폰: ");
-        	friendArray[i].setHp(sc.nextLine());
-        	
-        	System.out.println("\n학교: ");
-        	friendArray[i].setSchool(sc.nextLine());
-        	
-        	System.out.println("---------------------------");
-        }
-            
-        	// 친구정보(이름) 입력받기
+        	 for(int j=0; j<i; j++) {
+             	System.out.print("이름: ");
+             	friend.setName(sc.nextLine());
+             	
+             	System.out.print("핸드폰: ");
+             	friend.setHp(sc.nextLine());
+             	
+             	System.out.print("학교: ");
+             	friend.setSchool(sc.nextLine());
+             
+             	System.out.println("---------------------------");
 
+             }
+        	
+        }
+       
         	// 친구정보(핸드폰) 입력받기
         
         	// 친구정보(학교) 입력받기
@@ -36,10 +43,13 @@ public class FriendApp {
             // 인스턴스 주소를 배열에 대입
 
         
-        // 친구정보 출력
+        // 친구정보 출력 --> 계속 마지막 배열값만 출력됨.
+        
         for (int i = 0; i < friendArray.length; i++) {
             friendArray[i].showInfo();
         }
+        
+        
 
         sc.close();
     }
