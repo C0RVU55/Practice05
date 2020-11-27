2cd2d263bcde2a87d80b6bbd7ef1b40b95781cc4
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-27 10:27:41 +0900

    class_method_예제

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -3,10 +3,25 @@ package com.javaex.ex08;
 public class Account {
 
     private String accountNo;
-    private int balance;
+    private int balance; //자동으로 0으로 인식. 근데 static이 아닌데 왜 변한 값이 공유되는지.
     
     //생성자 작성
+    public Account(String accountNo) {
+    	this.accountNo=accountNo;
+    }
+    public Account(int balance) {
+    	this.balance=balance;
+    }
     
     //필요한 메소드 작성
-
+    public void deposit(int money) {
+    	balance=balance+money;
+    }
+    public void withdraw(int money) {
+    	balance=balance-money;
+    }
+    public void showBalance() {
+    	System.out.println(balance);
+    }
+    
 }
