e2be326939737c712bfdfcaf449c1d02535a1bfe
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-27 12:04:51 +0900

    class_method_예제

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,9 +2,17 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
+    public static String concatString(String[] strArray){
        
         //메소드 내용작성
+    	String result=""; //지역변수는 초기화해줘야 함.
+    	
+    	for(int i=0; i<strArray.length; i++) {
+    		String str=strArray[i];
+    		result += strArray[i]; // += <-- 기존 값에 우측항을 더함. 배열값을 각각 result 안에 넣어서 붙임.
+    	}
+    	
+    	return result;
         
     }
 
