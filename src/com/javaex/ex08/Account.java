b23afba70b1dc865194f95fde92bd0157a865809
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-29 14:53:25 +0900

    class_method_예제

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -3,7 +3,8 @@ package com.javaex.ex08;
 public class Account {
 
     private String accountNo;
-    private int balance; //자동으로 0으로 인식. 근데 static이 아닌데 왜 변한 값이 공유되는지.
+    private int balance; //자동으로 0으로 인식. 
+    //근데 static이 아닌데 왜 변한 값이 공유되는지 --> main에서 생성자로 받은 값을 switch반복문으로 계속 써서 변한 값이 저장되는듯. 
     
     //생성자 작성
     public Account(String accountNo) {
